52d9564cb497c3170960bc0a01dd8b863c9479f0
Pan Demosthenous <pan@vdrop.com>
2017-10-26 18:20:20 +1100

    fix typings

--- src/server.ts
+++ src/server.ts
@@ -111,11 +111,11 @@ export class SubscriptionServer {
    */
   private onUnsubscribe: Function;
 
-  public static create(options: ServerOptions, socketOptions: WebSocket.IServerOptions) {
+  public static create(options: ServerOptions, socketOptions: WebSocket.ServerOptions) {
     return new SubscriptionServer(options, socketOptions);
   }
 
-  constructor(options: ServerOptions, socketOptions: WebSocket.IServerOptions) {
+  constructor(options: ServerOptions, socketOptions: WebSocket.ServerOptions) {
     const {
       onSubscribe, onUnsubscribe, onOperation,
       onOperationComplete, onConnect, onDisconnect, keepAlive,
